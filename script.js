aa825064a27934dd36b657f00547355c3954586b
Paladugu <rpaladug@amazon.com>
2024-07-20 19:56:41 +0530

    updated

--- script.js
+++ script.js
@@ -4,52 +4,44 @@ let arr = [
     { id: 3, name: "karen", age: "19", profession: "admin" },
   ];
   
-  function PrintDeveloperwithMap() {
   function PrintDeveloperbyMap() {
     //Write your code here , just console.log
-    arr.map(employee => {
-        if(employee.profession == "developer" )
-        console.log(employee)
-  })
+    arr.map((employee) => {
+      if (employee.profession == "developer") {
+        console.log(employee);
+      }
+    });
   }
-  PrintDeveloperbyMap()
   
   function PrintDeveloperbyForEach() {
     //Write your code here , just console.log
-    arr.forEach(employee => {
-        if(employee.profession == "developer")
-            console.log(employee)
-    })
+    arr.forEach((employee) => {
+      if (employee.profession == "developer") {
+        console.log(employee);
+      }
+    });
   }
-  PrintDeveloperbyForEach()
   
   function addData() {
     //Write your code here, just console.log
-    let arr2 = {id:4,name:"susan",age:"20",profession:"intern"}
-    arr.push(arr2)
-    console.log(arr)
+    let newEmployee = { id: 4, name: "susan", age: "20", profession: "intern" };
+    arr.push(newEmployee);
+    console.log(arr);
   }
-  addData()
   
   function removeAdmin() {
     //Write your code here, just console.log
-    arr = arr.filter(employee => employee.profession !== "admin")
-            console.log(arr)
-    //console.log(arr)
+    arr = arr.filter((employee) => employee.profession != "admin");
+    console.log(arr);
   }
-  removeAdmin()
   
   function concatenateArray() {
     //Write your code here, just console.log
-    let arr1 = [
-          { id: 4, name: "suhas", age: "23", profession: "developer" },
-          { id: 5, name: "mike", age: "22", profession: "manager" },
-          { id: 6, name: "emily", age: "21", profession: "developer" }
-      ];
-      let concatenatedArray = arr.concat(arr1)
-      console.log(concatenatedArray)
+    let newArr = [
+      { id: 4, name: "alice", age: "22", profession: "manager" },
+      { id: 5, name: "bob", age: "21", profession: "developer" },
+      { id: 6, name: "charlie", age: "23", profession: "designer" },
+    ];
+    let concatenatedArray = arr.concat(newArr);
+    console.log(concatenatedArray);
   }
-  concatenateArray()
-  
-  }
-  PrintDeveloperwithMap()
